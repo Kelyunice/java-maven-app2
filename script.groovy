d2fd6a6669bfa3997fd16c5ec9138156ac513431
Kelyunice <bongnjiokely14@gmail.com>
2025-01-08 23:35:14 +0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t kelyunice1419:version1 .'
+        sh 'docker build -t kelyunice1419:jma1.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push kelyunice1419:version1'
+        sh 'docker push kelyunice1419:jma1.0'
     }
 }
 
